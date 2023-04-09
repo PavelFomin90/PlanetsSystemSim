21bcff48b0fa5f45811410a9f2b4e673543bb9ac
Pavel Fomin <me@pavelfomin.ru>
2023-04-09 19:11:37 +0300

    PS-2 Linters (#2)
    * PS-1 initial commit
    
    * PS-1 remove vs settings
    
    * PS-1 fix
    
    * PS-2 linters
    
    ---------
    
    Co-authored-by: pfomin <pfomin@ivi.ru>

--- src/controller/translateController.ts
+++ src/controller/translateController.ts
@@ -1,170 +1,168 @@
-import { round } from '../utils/math';
-import { throttle } from '../utils/throttle';
+import { round } from "../utils/math";
+import { throttle } from "../utils/throttle";
 
 interface ITranslateController {
-    scale: number;
-    translate: { x: number, y: number }
+  scale: number;
+  translate: { x: number; y: number };
 }
 
 interface ITranslateControllerProps {
-    initialScale: number;
-    initialTranslate: { x: number, y: number };
-};
+  initialScale: number;
+  initialTranslate: { x: number; y: number };
+}
 
 const INTERVAL_TIME = 20;
 
-class TranslateController implements ITranslateController{
-    scale = 0.2;
-    translate = { x: 0, y: 0 };
+class TranslateController implements ITranslateController {
+  scale = 0.2;
+  translate = { x: 0, y: 0 };
 
-    private static _instance: TranslateController;
-    private interval: ReturnType<typeof setInterval> | undefined;
+  private static _instance: TranslateController;
+  private interval: ReturnType<typeof setInterval> | undefined;
 
-    constructor(props: ITranslateControllerProps) {
+  constructor(props: ITranslateControllerProps) {
+    if (TranslateController._instance) {
+      return TranslateController._instance;
+    }
 
-        if (TranslateController._instance) {
-            return TranslateController._instance;
+    this.scale = props.initialScale;
+    this.translate = props.initialTranslate;
+
+    this.setListner();
+
+    TranslateController._instance = this;
+  }
+
+  private scaleFunction = (scaleDelta: number) => {
+    this.interval && clearInterval(this.interval);
+    const initScale = this.scale;
+    let intervalFunc;
+    if (scaleDelta > 0) {
+      intervalFunc = () => {
+        this.scale += scaleDelta / 10;
+        if (this.scale >= initScale + scaleDelta) {
+          clearInterval(this.interval);
         }
-
-        this.scale = props.initialScale;
-        this.translate = props.initialTranslate
-
-        this.setListner();
-
-        TranslateController._instance = this;
-    }
-
-    private scaleFunction = (scaleDelta: number) => {
-        this.interval && clearInterval(this.interval);
-        const initScale = this.scale;
-        let intervalFunc;
-        if (scaleDelta > 0) {
-            intervalFunc = () => {
-                this.scale += scaleDelta / 10;
-                if (this.scale >= initScale + scaleDelta) {
-                    clearInterval(this.interval);
-                }
-            }
-        } else {
-            if (this.scale <= 0.05) {
-                clearInterval(this.interval);
-                return;
-            }
-            intervalFunc = () => {
-                this.scale += scaleDelta / 10;
-                if (this.scale <= initScale + scaleDelta) {
-                    clearInterval(this.interval);
-                }
-            }
+      };
+    } else {
+      if (this.scale <= 0.05) {
+        clearInterval(this.interval);
+        return;
+      }
+      intervalFunc = () => {
+        this.scale += scaleDelta / 10;
+        if (this.scale <= initScale + scaleDelta) {
+          clearInterval(this.interval);
         }
-
-        this.interval = setInterval(intervalFunc, INTERVAL_TIME);
-    };
-
-    private scaleIn = () => {
-        this.scaleFunction(0.01);
+      };
     }
 
-    private scaleOut = () => {
-        this.scaleFunction(-0.01);
+    this.interval = setInterval(intervalFunc, INTERVAL_TIME);
+  };
+
+  private scaleIn = () => {
+    this.scaleFunction(0.01);
+  };
+
+  private scaleOut = () => {
+    this.scaleFunction(-0.01);
+  };
+
+  private moveLeft = () => {
+    this.interval && clearInterval(this.interval);
+    const initialTranslateX = this.translate.x;
+
+    this.interval = setInterval(() => {
+      this.translate.x = round(this.translate.x - 2);
+      if (this.translate.x <= initialTranslateX - 20) {
+        clearInterval(this.interval);
+      }
+    }, INTERVAL_TIME);
+  };
+
+  private moveRight = () => {
+    this.interval && clearInterval(this.interval);
+    const initialTranslateX = this.translate.x;
+
+    this.interval = setInterval(() => {
+      this.translate.x = round(this.translate.x + 2);
+      if (this.translate.x >= initialTranslateX + 20) {
+        clearInterval(this.interval);
+      }
+    }, INTERVAL_TIME);
+  };
+
+  private moveTop = () => {
+    this.interval && clearInterval(this.interval);
+    const initialTranslateY = this.translate.y;
+
+    this.interval = setInterval(() => {
+      this.translate.y = round(this.translate.y + 2);
+      if (this.translate.y >= initialTranslateY + 20) {
+        clearInterval(this.interval);
+      }
+    }, 20);
+  };
+
+  private moveDown = () => {
+    this.interval && clearInterval(this.interval);
+    const initialTranslateY = this.translate.y;
+
+    this.interval = setInterval(() => {
+      this.translate.y = round(this.translate.y - 2);
+      if (this.translate.y <= initialTranslateY - 20) {
+        clearInterval(this.interval);
+      }
+    }, 20);
+  };
+
+  private scaleWithMouse = (deltaY: number) => {
+    let scaleDelta;
+    const invertDeltaY = deltaY * -1;
+
+    if (deltaY > 0) {
+      scaleDelta = Math.max(0.02, invertDeltaY / 20000);
+    } else {
+      scaleDelta = Math.min(-0.02, invertDeltaY / 20000);
     }
 
-    private moveLeft = () => {
-        this.interval && clearInterval(this.interval);
-        const initialTranslateX = this.translate.x;
+    this.scaleFunction(scaleDelta);
+  };
 
-        this.interval = setInterval(() => {
-            this.translate.x = round(this.translate.x - 2);
-            if (this.translate.x <= initialTranslateX - 20) {
-                clearInterval(this.interval);
-            }
-        }, INTERVAL_TIME);
-    };
+  private setListner = () => {
+    const throttledScale = throttle<number>((deltaY) => {
+      deltaY && this.scaleWithMouse(deltaY);
+    }, 200);
 
-    private moveRight = () => {
-        this.interval && clearInterval(this.interval);
-        const initialTranslateX = this.translate.x;
-
-        this.interval = setInterval(() => {
-            this.translate.x = round(this.translate.x + 2);
-            if (this.translate.x >= initialTranslateX + 20) {
-                clearInterval(this.interval);
-            }
-        }, INTERVAL_TIME);
-    };
-
-    private moveTop = () => {
-        this.interval && clearInterval(this.interval);
-        const initialTranslateY = this.translate.y;
-
-        this.interval = setInterval(() => {
-            this.translate.y = round(this.translate.y + 2);
-            if (this.translate.y >= initialTranslateY + 20) {
-                clearInterval(this.interval);
-            }
-        }, 20);
-    };
-
-    private moveDown = () => {
-        this.interval && clearInterval(this.interval);
-        const initialTranslateY = this.translate.y;
-
-        this.interval = setInterval(() => {
-            this.translate.y = round(this.translate.y - 2);
-            if (this.translate.y <= initialTranslateY - 20) {
-                clearInterval(this.interval);
-            }
-        }, 20);
-    };
-
-    private scaleWithMouse = (deltaY: number) => {
-        let scaleDelta;
-        const invertDeltaY = deltaY * -1;
-
-        if (deltaY > 0) {
-            scaleDelta = Math.max(0.02, invertDeltaY / 20000);
-        } else {
-            scaleDelta = Math.min(-0.02, invertDeltaY / 20000);
-        }
-
-        this.scaleFunction(scaleDelta);
-    }
-
-    private setListner = () => {
-        const throttledScale = throttle<number>((deltaY) => {
-            deltaY && this.scaleWithMouse(deltaY);
-        }, 200);
-
-        document.addEventListener('wheel', (e) => {
-            throttledScale(e.deltaY);
-        });
-        document.addEventListener('keydown', (e) => {
-            switch (e.keyCode) {
-                case 81:
-                    this.scaleOut();
-                    break;
-                case 87:
-                    this.scaleIn();
-                    break;
-                case 37:
-                    this.moveRight();
-                    break;
-                case 38:
-                    this.moveTop();
-                    break;
-                case 39:
-                    this.moveLeft();
-                    break;
-                case 40:
-                    this.moveDown();
-                    break;
-                default:
-                    clearInterval(this.interval);
-                    break;
-            }
-        })
-    }
-
+    document.addEventListener("wheel", (e) => {
+      throttledScale(e.deltaY);
+    });
+    document.addEventListener("keydown", (e) => {
+      switch (e.keyCode) {
+        case 81:
+          this.scaleOut();
+          break;
+        case 87:
+          this.scaleIn();
+          break;
+        case 37:
+          this.moveRight();
+          break;
+        case 38:
+          this.moveTop();
+          break;
+        case 39:
+          this.moveLeft();
+          break;
+        case 40:
+          this.moveDown();
+          break;
+        default:
+          clearInterval(this.interval);
+          break;
+      }
+    });
+  };
 }
 
 export { TranslateController };
--- src/utils/throttle.ts
+++ src/utils/throttle.ts
@@ -1,22 +1,19 @@
 const throttle = <A>(callback: (args?: A) => unknown, timeout: number) => {
-    let freeze = false;
+  let freeze = false;
 
-    return (args?: A) => {
-        let timeoutFunc: ReturnType<typeof setTimeout>;
+  return (args?: A) => {
+    if (freeze) {
+      return;
+    }
 
-        if (freeze) {
-            return;
-        }
+    freeze = true;
+    callback(args);
 
-        freeze = true;
-        callback(args);
-    
-        timeoutFunc = setTimeout(() => {
-            freeze = false;
-            clearTimeout(timeoutFunc);
-        }, timeout);
-    };
+    const timeoutFunc = setTimeout(() => {
+      freeze = false;
+      clearTimeout(timeoutFunc);
+    }, timeout);
+  };
 };
-
 
 export { throttle };
